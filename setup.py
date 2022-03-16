014c6a9723a0829b9260c1a5fa374065ccee5845
Jacob Gerlach <jwgerlach00@gmail.com>
2022-03-16 19:40:16 -0400

    version 0.0.5 -- testing stse import

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stse',
-    version='0.0.4',
+    version='0.0.5',
     license='MIT',
     author='Jacob Gerlach',
     author_email='jwgerlach00@gmail.com',
