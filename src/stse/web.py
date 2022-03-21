12f459821118d99d707b76129afd35ae217f72d5
jacob <jwgerlach00@gmail.com>
2022-03-21 16:22:56 -0400

    added to web.py and web tests

--- src/stse/web.py
+++ src/stse/web.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import warnings
+from werkzeug.datastructures import FileStorage
+import tempfile
+import mimetypes
 
 
-def read_spreadsheet(file, return_ext=False):
+def read_spreadsheet(file, return_ext=False):  # *
 
     # Get file MIME
     file_mime = file.content_type
@@ -35,3 +38,23 @@ def read_spreadsheet(file, return_ext=False):
 
 
     return (input_data, file_ext) if return_ext else input_data
+
+def store_df(df, ext):
+    
+    buffer = tempfile.NamedTemporaryFile()
+    if ext == 'csv':
+        df.to_csv(buffer, index=False)
+    elif ext == 'xlsx':
+        df.to_excel(buffer, index=False)
+    elif ext == 'tsv':
+        df.to_csv(buffer, index=False, sep='\t')
+    else:
+        raise('Only "csv", "xlsx" and "tsv" extensions are accepted')
+    
+    type = mimetypes.guess_type(f'_.{ext}')[0]
+    
+    return FileStorage(
+            stream=open(buffer.name, 'rb'),
+            filename=f'test.{ext}',
+            content_type=type,
+    )
--- src/test/test_web.py
+++ src/test/test_web.py
@@ -1,36 +1,45 @@
+import unittest
 from werkzeug.datastructures import FileStorage
 import io
 import json
 import pandas as pd
 import json
+import tempfile
+from pandas import testing
 
 from stse import web
 
-# Here we are mocking a JSON file called Input.json
-my_dict = 'testing'
-df = pd.DataFrame({
-    'a': [1, 2, 3, 4],
-    'b': ['a', 'b', 'c', 'd']
-})
-
-print(df)
-
-buffer = io.BytesIO()
-(packbionics) jacob@jacob-XPS-13:~/git_repos/state_prediction_ml$ 
-# df.to_json('data/web_test.json')
-df.to_csv(buffer)
-
-print(buffer)
-
 # input_json = json.dumps(my_dict, indent=4).encode("utf-8")
 
-mock_file = FileStorage(
-    stream=buffer,
-    filename='ape.csv',
-    content_type='text/csv',
-)
-# print(mock_file)
-print(pd.read_json('data/web_test.json'))
+# mock_file = FileStorage(
+#     stream=open(buffer.name, 'rb'),
+#     filename='ape.csv',
+#     content_type='text/csv',
+# )
+
 # print(web.read_spreadsheet(mock_file))
-print(mock_file)
-print(mock_file.read())
+
+class TestWeb(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.df = pd.DataFrame({
+            'a': [1, 2, 3, 4],
+            'b': ['a', 'b', 'c', 'd']
+        })
+        return super().setUpClass()
+    
+    def test_read_shreadsheet(self):
+        # Test csv
+        mock_file = web.store_df(self.df, 'csv')
+        testing.assert_frame_equal(web.read_spreadsheet(mock_file), self.df)
+        
+        # Test excel
+        mock_file = web.store_df(self.df, 'xlsx')
+        testing.assert_frame_equal(web.read_spreadsheet(mock_file), self.df)
+        
+        # Test csv
+        mock_file = web.store_df(self.df, 'tsv')
+        testing.assert_frame_equal(web.read_spreadsheet(mock_file), self.df)
+        
+    # def test_store_df(self):
+        
