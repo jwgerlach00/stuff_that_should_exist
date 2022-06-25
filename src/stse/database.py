8c2327d7d0d83c3c8cf819f740f14b965184cd3b
jacob <jwgerlach00@gmail.com>
2022-06-25 11:49:55 -0400

    unit tests for database

--- src/stse/database.py
+++ src/stse/database.py
@@ -3,29 +3,18 @@ from pymongo.collection import Collection
 import json
 
 
-def df_2_db(df:pd.DataFrame, collection:Collection) -> None:
+def df_2_db(df:pd.DataFrame, collection:Collection) -> None:  # *
     """Inserts dataframe into mongo Collection.
 
     Args:
         df (pd.DataFrame): Data to insert into collection.
-        collection (Collection): Collection to insert data into.
+        collection (Collection): Collection object to insert data into.
     """
     data_dict = df.to_dict('records')
     collection.insert_many(data_dict)
     
-def df_2_json(df:pd.DataFrame) -> str:
-    """Converts from dataframe to JSON string.
-
-    Args:
-        df (pd.DataFrame): Data.
-
-    Returns:
-        str: JSON string data.
-    """
-    return json.dumps(df.to_numpy().tolist())
-    
-def db_2_df(collection:Collection) -> pd.DataFrame:
-    """Extracts all data from collection and returns as pandas DataFrame.
+def db_2_df(collection:Collection) -> pd.DataFrame:  # *
+    """Extracts all data from collection and returns as single pandas DataFrame.
 
     Args:
         collection (Collection): Collection to grab all data from.
